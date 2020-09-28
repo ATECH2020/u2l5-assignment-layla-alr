d59b4d1cf7041c2e8f0ab86b95154b54c55c22f5
Layla Al-Rashed <layla.1219282@nv.ccsd.net>
2020-09-28 03:22:49 +0000

    Construction costs completed.

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,26 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner in = new Scanner(System.in);
+        int numBoards; 
+        int numWindows; 
+        double lumCost = 8.0;
+        double windCost= 11.0;
+        double taxRate;
+        double total; 
+
+        //get user input
+        System.out.println("How many boards will you need?");
+        numBoards = in.nextInt();
+        System.out.println("How many windows will you need?");
+        numWindows= in.nextInt();
+        System.out.println("What's the tax rate?");
+        taxRate= in.nextDouble();
+
+        //Create object and Print total and grand total
+        Construction user = new Construction(lumCost, windCost, taxRate);
+        total = user.lumberCost(numBoards) + user.windowCost(numWindows);
+        System.out.println("Total: "+ total);
+        System.out.print("Grand total: "+ user.grandTotal(total));
     }
 }
